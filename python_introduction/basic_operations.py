f663e3f295f07e093bc4f0d4c4071685d6c8dfa6
rolandslt <lulangoroland46@gmail.com>
2025-09-09 01:33:00 +0200

    accomplish basic arithmetic operations in Python by performing predefined calculations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+#Define the variables
+number1 = 10
+number2 = 5
+
+#Calculate the sum
+sum_result = number1 + number2
+
+# Calculate the difference 
+difference_result = number1 -number2 
+
+# Calculate the product 
+product_result = number1 * number2 
+
+# print the results 
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
