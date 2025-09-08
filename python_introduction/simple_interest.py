f674ad406a9295092b6a2a6b42c3d6159ea4a5f3
rolandslt <lulangoroland46@gmail.com>
2025-09-09 01:53:37 +0200

    Apply basic Python arithmetic operations and variable assignments to calculate the simple interest on a given principal amount, rate of interest, and time.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+#Define the three values principal(P), rate(R) adn time(T)
+P = 1000
+R = 0.05
+T = 3
+I = "interest"
+
+# calculation of the interest earned 
+I = P * R * T 
+
+# print the calculated interest 
+print(f"principal = {P}")
+print(f"rate = {R}")
+print(f"time = {T}")
+print(f"The simple interest is: {I}")
