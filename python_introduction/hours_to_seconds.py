966f24639314f280036d8cc5dd0161689c67f01b
rolandslt <lulangoroland46@gmail.com>
2025-09-09 02:10:27 +0200

    using arithmetic operations by converting a given number of hours into seconds.

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+#Define variable hours and assign it to its value 
+hours = 2 
+
+# Calculate the number of seconds in the given hours
+seconds = hours * 3600
+
+#print he value of seconds 
+print(f"{hours} hour(S) is {seconds} seconds")
