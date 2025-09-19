3efc87587502d75a67e0a95448ce0634cecc18ec
rolandslt <lulangoroland46@gmail.com>
2025-09-19 13:01:02 +0200

    correct the configuration of the loop

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,5 @@
-rows = 10 
+
 number = int(input("Enter a number to see its multiplication table:"))
-for Y in range(1, rows + 1) :
-    Z = number * Y
-    print(f"{number} * {Y} = {Z}")
+for Y in range(1, 10+ 1) :
+    print(f"{number} * {Y} = {number * Y}")
 print() 
