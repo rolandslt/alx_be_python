8cd51145e761f7927198f06d43103f22eb2e8a4f
rolandslt <lulangoroland46@gmail.com>
2025-10-15 01:12:58 +0200

    Correct __str__ output formatting for all book classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author 
     def __str__(self):
-        return f"Book : {self.title} by {self.author}" 
+        return f"Book: {self.title} by {self.author}" 
 class EBook(Book):
     
     def __init__(self, title: str , author: str , file_size: int):
@@ -19,7 +19,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
     def __str__(self):
-        return f"PrintBook: {self.title}, {self.author}, {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, {self.page_count}"
 
 class  Library:
     def __init__(self):
